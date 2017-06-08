a8920f98efe0a5a73dd1c9284e84dd7f99b65836
Sarah Veness <sarahveness@omnifilm.com>
2017-06-08 16:18:03 -0700

    fully responsive, rent page nearly finished, need to add mobile display and text stylings

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,15 @@
   $(document).ready(function() {
-    $('#fullpage').fullpage({
-      scrollingSpeed: 1000,
-      loopHorizontal: false
-
-    });
-  });
+	$('#fullpage').fullpage({
+    scrollingSpeed: 1000,
+    loopHorizontal: false,
+    controlArrows: true,
+		verticalCentered: true,
+		sectionsColor : ['#ccc', '#fff'],
+		// fixedElements: '#header, .footer',
+		responsiveWidth: 0,
+		responsiveHeight: 0,
+		responsiveSlides: false,
+		parallax: false,
+		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'}
+	});
+});
